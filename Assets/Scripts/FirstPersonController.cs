5ede12c36a40f6c59b7c09a46c23383c7cd02c0a
Saurabh Jhanjee <Saurabh Jhanjee>
2016-10-29 20:43:01 +1100

    FPS Controller and stuff

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+	public int playerspeed = 2;
+	public int sens = 2;
+	public int jumpSpeed = 5;
+	float vrot = 0;
+	public int vrange = 60;
+	float vvelocity = 0;
+
+	CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		characterController = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		// Camera switching
+
+			float hrot = Input.GetAxis ("Mouse X") * sens;
+			transform.Rotate (0, hrot, 0);
+
+			vrot -= Input.GetAxis ("Mouse Y") * sens;
+
+			vrot = Mathf.Clamp (vrot, -vrange, vrange);
+
+			Camera.main.transform.localRotation = Quaternion.Euler (vrot, 0, 0);
+
+
+			// Movement
+
+			float forwardSpeed = Input.GetAxis ("Vertical") * playerspeed;
+			float strafe = Input.GetAxis ("Horizontal") * playerspeed;
+
+			vvelocity += Physics.gravity.y * Time.deltaTime;
+
+			if (characterController.isGrounded && Input.GetButton ("Jump")) {
+				vvelocity = jumpSpeed;
+			}
+
+			Vector3 speed = new Vector3 (strafe, vvelocity, forwardSpeed);
+
+			speed = transform.rotation * speed;
+
+			characterController.Move (speed * Time.deltaTime);
+		}
+	}
