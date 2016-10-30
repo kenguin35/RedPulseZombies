a0424f3272bc23b4f43f2845c15702169cc8b37c
indegame <indegamedev@gmail.com>
2016-10-30 12:12:08 +1100

    Put in gun, and raycasting

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	public double shootInterval = 0.5;
+	public GameObject rayOrigin;
+	LineRenderer lineRenderer;
+
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+	}
+	
+
+	void FixedUpdate () {
+		if (Input.GetMouseButton (0)) {
+			Shoot ();
+		}	
+	}
+
+	void Shoot(){
+		
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		Physics.Raycast (Camera.main.transform.position, fwd, 20);
+
+	}
+}
