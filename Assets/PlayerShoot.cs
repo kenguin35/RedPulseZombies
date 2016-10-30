f4acee4c90f13de2bf3e46a707353697f3e02866
Ryan <ryanwp526@gmail.com>
2016-10-30 13:18:49 +1100

    shooting timer

--- Assets/PlayerShoot.cs
+++ Assets/PlayerShoot.cs
@@ -3,18 +3,26 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
-	public double shootInterval = 0.5;
+	public double shootInterval = 0.5d;
 	public GameObject rayOrigin;
 	LineRenderer lineRenderer;
 
 	void Start () {
 		lineRenderer = GetComponent<LineRenderer> ();
 	}
-	
 
+    void Update()
+    {
+        if (shootInterval > 0)
+        {
+            shootInterval -= Time.deltaTime;
+        }
+
+    }
 	void FixedUpdate () {
-		if (Input.GetMouseButton (0)) {
+        if ((Input.GetMouseButton(0) && shootInterval <= 0)) {
 			Shoot ();
+            shootInterval = 0.5d;
 		}	
 	}
 
